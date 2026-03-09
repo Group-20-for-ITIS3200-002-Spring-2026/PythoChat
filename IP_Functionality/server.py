0a08e0bd5f8ced3bdd87ae421e8852b64973cf01
James Karwowski-Mulready <james.karwowski@outlook.com>
2026-03-09 10:12:09 -0400

    Implement data receiving function in server.py
    Refactor server script structure and add data receiving functionality. Need to implement server function for setting up a connection to client.

---
+++ IP_Functionality/server.py
@@ -0,0 +1,32 @@
+# Found these based on research of Python libraries. May/May Not Use all of them.
+import socket
+import threading
+import sys
+import random
+
+# Import external scripts here:
+# Example: import other_script
+
+def recieve_data():
+  # Handles recieving data from client
+  while True:
+    try:
+      data = conn.recv(1024)
+      if not data:
+        break
+      # Logic for determining type of data. Requires the use of external scripts to decrypt messages.
+      # For testing we will use text first.
+      print(f"\n[Client]: {data}")
+      print("Your Message: ", end='', flush=True)
+    except:
+      break
+  print("Client has disconnected.")
+  conn.close()
+
+# Host value is IP of the user's input.
+# May use port value as a randomly-generated value above 1024 and at most 65000.
+def server_script():
+  # To be implemented.
+
+if __name__ = "__main__":
+  reciever_script()
