5b7d3c0b9fd93ced72f1fe6a6a106efa23417ddd
James Karwowski-Mulready <james.karwowski@outlook.com>
2026-04-06 12:22:24 -0400

    Implement Diffie-Hellman key exchange in server

--- IP_Functionality/server.py
+++ IP_Functionality/server.py
@@ -3,19 +3,38 @@ import socket
 import threading
 import struct
 from plyer import notification
-from text_encryption import encrypt_message, decrypt_message, derive_key
+from text_encryption import encrypt_message, decrypt_message, derive_key, generate_public_key, generate_shared_key
 from image_encryption import encrypt_image, decrypt_image
 from PIL import Image
 
 # Import external scripts here:
 # Example: import other_script
 
-SHARED_KEY = derive_key(123456)
-
 clients = []
 
 def handle_client(conn, addr):
   print(f"Client {addr} connected.")
+
+  # --- DH HANDSHAKE ---
+  header = recv_exact(conn, 12)
+  msg_type, length = struct.unpack("!4sQ", header)
+
+  data = recv_exact(conn, length)
+  p, g, client_public = map(int, data.decode().split(','))
+
+  private_key = random.randint(1, 100)
+  public_key = generate_public_key(private_key, g, p)
+
+  # Send B
+  response = str(public_key).encode()
+  header = struct.pack("!4sQ", b"DHKE", len(response))
+  conn.sendall(header + response)
+
+  shared = generate_shared_key(client_public, private_key, p)
+  shared_key = derive_key(shared)
+
+  print(f"Secure channel established with {addr}")
+  
   clients.append(conn)
 
   while True:
@@ -63,16 +82,27 @@ def handle_client(conn, addr):
   conn.close()
   print(f"Client {addr} disconnected.")
 
-def broadcast(sender_conn, msg_type, data):
-  header = struct.pack("!4sQ", msg_type.encode(), len(data))
-  packet = header + data
-  
-  for client in clients:
-    if client != sender_conn:
-      try:
-        client.sendall(packet)
-      except:
-        pass
+def broadcast(sender, msg_type, data):
+    for client, key in clients:
+      if client != sender:
+          try:
+              if msg_type == "TEXT":
+                  payload = data[:-32]
+                  h = data[-32:]
+                  message = decrypt_text(payload, h, key)
+
+                  new_payload, new_h = encrypt_text(message, key)
+                  new_data = new_payload + new_h
+
+              else:
+                  # images already encrypted; forward directly
+                  new_data = data
+
+                header = struct.pack("!4sQ", msg_type.encode(), len(new_data))
+                client.sendall(header + new_data)
+
+          except:
+              pass
 
 # Host value is IP of the user's input.
 # May use port value as a randomly-generated value above 1024 and at most 65000.
